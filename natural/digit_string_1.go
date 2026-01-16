d67a21a15dbea4d2d968ba5ce7c0f3aa72d36592
SebastianLorenz <sebastian.lorenz.1@gmx.de>
2026-01-16 22:20:24 +0100

    einund

--- natural/digit_string_1.go
+++ natural/digit_string_1.go
@@ -12,6 +12,29 @@ package natural
 // um den Gesamt-String einer Zahl zusammenzusetzen.
 // Diese Funktion muss nur für den Normalfall (Zahlen >= 21) funktionieren.
 func DigitString1(digit int) string {
-	// TODO
-	return ""
+
+	if digit == 1 {
+		return "einund"
+	} else if digit == 2 {
+		return "zweiund"
+	} else if digit == 3 {
+		return "dreiund"
+	} else if digit == 4 {
+		return "vierund"
+	} else if digit == 5 {
+		return "fünfund"
+	} else if digit == 6 {
+		return "sechsund"
+	} else if digit == 7 {
+		return "siebenund"
+	} else if digit == 8 {
+		return "achtund"
+	} else if digit == 9 {
+		return "neunund"
+	} else if digit == 0 {
+		return ""
+	} else {
+		return "Fehler"
+	}
+
 }
